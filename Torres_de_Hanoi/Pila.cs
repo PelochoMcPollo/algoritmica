253794eff71a2d15d71040347ae4d701cd58daf6
PelochoMcPollo <eduard01kate@gmail.com>
2023-03-13 19:22:45 +0100

    Clase Pila

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -9,33 +9,53 @@ namespace Torres_de_Hanoi
     class Pila
     {
         public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
+ 
         public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
-        public List<Disco> Elementos { get; set; }
-        */
 
-        /* TODO: Implementar métodos */
+        public List<Disco> Elementos { get; set; } = new List<Disco>();
         public Pila()
         {
+            this.Size = 0;
+            this.Top = 0;
+        }
+
+        public Pila(int n)
+        {
+            for (int i = n; i > 0; i--)
+            {
+                Disco disco = new Disco();
+                disco.Valor = i;  
+              this.Elementos.Add(disco);
+            }
+            this.Size = n;
+            this.Top = this.Elementos[this.Size-1].Valor;
 
         }
 
         public void push(Disco d)
         {
-
+            this.Elementos.Add(d);
+            this.Size++;
+            this.Top = d.Valor;
         }
+
 
         public Disco pop()
         {
-            return null;
+            Disco disco = new Disco();
+            disco = this.Elementos[this.Size - 1];
+            this.Elementos.Remove(disco);
+            this.Size--;
+            this.Top=0;
+            if (this.Size != 0) {
+                this.Top = this.Elementos[this.Size - 1].Valor;
+            }
+            return disco;
         }                
 
         public bool isEmpty()
         {
+            if(this.Size != 0) return false;
             return true;
         }
 
